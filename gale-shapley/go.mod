1947b0520309602b76fb8bc13a0da1883f5b163b
arc <zaveyborodaid@gmail.com>
2025-11-02 19:09:24 +0500

    Rename directory

--- gale-shapley/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ayaxdd/algo-design/gale-shapley
-
-go 1.25.0
--- galeshapley/algo.go
+++ galeshapley/algo.go
@@ -1,6 +1,10 @@
-package main
+// Package galeshapley
+package galeshapley
 
-import "container/list"
+import (
+	"container/list"
+	"math/rand"
+)
 
 type GaleShapley struct {
 	n         int
@@ -64,4 +68,36 @@ func (gc *GaleShapley) Execute() [][]int {
 	}
 
 	return marriage
+}
+
+func fillPref(pref [][]int) {
+	n := len(pref)
+	prefList := make([]int, n)
+	shuffleFunc := func(i, j int) {
+		prefList[i], prefList[j] = prefList[j], prefList[i]
+	}
+
+	for i := range n {
+		prefList[i] = i
+	}
+
+	for h := range n {
+		if pref[h] == nil {
+			rand.Shuffle(n, shuffleFunc)
+			pref[h] = append(pref[h], prefList...)
+		}
+	}
+}
+
+func fillRanking(ranking, womenPref [][]int) {
+	for w, men := range womenPref {
+		score := len(womenPref)
+		if ranking[w] == nil {
+			ranking[w] = make([]int, len(womenPref))
+		}
+		for _, m := range men {
+			ranking[w][m] = score
+			score--
+		}
+	}
 }
