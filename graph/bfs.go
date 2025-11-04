58f30636c15c304201d51e064995e2fe83e23cc7
arc <zaveyborodaid@gmail.com>
2025-11-04 19:09:33 +0500

    Fix nil errors

--- graph/bfs.go
+++ graph/bfs.go
@@ -4,24 +4,28 @@ package graph
 import "github.com/ayaxdd/algorithm-design/ds"
 
 func Bfs[T comparable](g *ds.Graph[T], s *ds.Node[T]) []*ds.Node[T] {
+	if g == nil || s == nil {
+		return nil
+	}
+
 	discovered := make(map[string]bool, g.VerticesCnt())
 	discovered[s.GetID()] = true
 
-	q := ds.NewQueue[*ds.Node[T]]()
-	q.Enqueue(s)
+	que := ds.NewQueue[*ds.Node[T]]()
+	que.Enqueue(s)
 
 	// i := 0
 	nodes := make([]*ds.Node[T], 0, 1)
 	nodes = append(nodes, s)
 
-	for !q.IsEmpty() {
-		u, _ := q.Dequeue()
+	for !que.IsEmpty() {
+		u, _ := que.Dequeue()
 		neighbours, _ := g.GetNeighbours(u)
 		for _, v := range neighbours {
 			if !discovered[v.GetID()] {
 				discovered[v.GetID()] = true
 				nodes = append(nodes, v)
-				q.Enqueue(v)
+				que.Enqueue(v)
 			}
 		}
 	}
