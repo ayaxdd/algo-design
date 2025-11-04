07631d50fd658cd837a8812e5c60445d59eab073
arc <zaveyborodaid@gmail.com>
2025-11-04 19:08:57 +0500

    Init DFS implementation

--- graph/dfs.go
+++ graph/dfs.go
@@ -1,1 +1,30 @@
 package graph
+
+import "github.com/ayaxdd/algorithm-design/ds"
+
+func Dfs[T comparable](g *ds.Graph[T], s *ds.Node[T]) []*ds.Node[T] {
+	if g == nil || s == nil {
+		return nil
+	}
+
+	explored := make(map[string]bool, g.VerticesCnt())
+
+	stack := ds.NewStack[*ds.Node[T]]()
+	stack.Push(s)
+
+	nodes := make([]*ds.Node[T], 0, 1)
+
+	for !stack.IsEmpty() {
+		u, _ := stack.Pop()
+		if !explored[u.GetID()] {
+			explored[u.GetID()] = true
+			nodes = append(nodes, u)
+			neighbours, _ := g.GetNeighbours(u)
+			for _, v := range neighbours {
+				stack.Push(v)
+			}
+		}
+	}
+
+	return nodes
+}
