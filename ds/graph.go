7f47af8704eccbdce15fd93645b3328d41fc3b7e
arc <zaveyborodaid@gmail.com>
2025-11-04 16:09:58 +0500

    Add GetNeighbours() implementation

--- ds/graph.go
+++ ds/graph.go
@@ -14,6 +14,14 @@ func NewNode[T comparable](id string, value T) *Node[T] {
 	}
 }
 
+func (n *Node[T]) GetID() string {
+	return n.id
+}
+
+func (n *Node[T]) String() string {
+	return fmt.Sprintf("(%s:%v)", n.id, n.value)
+}
+
 type Graph[T comparable] struct {
 	edges    map[string]map[string]int // u -> (v -> w)
 	vertices map[string]*Node[T]
@@ -26,13 +34,16 @@ func NewGraph[T comparable]() *Graph[T] {
 	}
 }
 
-func (g *Graph[T]) AddVertex(name string, value T) bool {
-	if _, exists := g.vertices[name]; exists {
+func (g *Graph[T]) AddVertex(v *Node[T]) bool {
+	if _, exists := g.vertices[v.id]; exists {
 		return false
 	}
-	n := NewNode(name, value)
-	g.vertices[name] = n
+	g.vertices[v.id] = v
 	return true
+}
+
+func (g *Graph[T]) VerticesCnt() int {
+	return len(g.vertices)
 }
 
 func (g *Graph[T]) AddEdge(u, v *Node[T], w int) {
@@ -48,6 +59,20 @@ func (g *Graph[T]) AddEdge(u, v *Node[T], w int) {
 	}
 
 	g.edges[u.id][v.id] = w
+}
+
+func (g *Graph[T]) GetNeighbours(n *Node[T]) ([]*Node[T], bool) {
+	neighboursMap := g.edges[n.id]
+	if neighboursMap == nil {
+		return nil, false
+	}
+	neighbours := make([]*Node[T], 0, len(neighboursMap))
+
+	for neighbour := range neighboursMap {
+		node := g.vertices[neighbour]
+		neighbours = append(neighbours, node)
+	}
+	return neighbours, true
 }
 
 func (g *Graph[T]) String() string {
