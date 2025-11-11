73f6565c0004e304f855cd4c29d04fc65a6ef9e3
arc <zaveyborodaid@gmail.com>
2025-11-11 10:56:33 +0500

    Implement Transpose() and Edges()
    Also vertices and edges size now tracks as Grpah's field

--- ds/graph.go
+++ ds/graph.go
@@ -35,10 +35,14 @@ func (e Edge[T]) String() string {
 	return fmt.Sprintf("<%v%s%v> w: %d", e.u, e.bound, e.v, e.w)
 }
 
+// TODO: Add graph iterator
+
 type Graph[T comparable] struct {
 	edges    map[T]map[T]int
 	vertices map[T]*Node[T]
 	directed bool
+	vCnt     int
+	eCnt     int
 	// weighted bool
 }
 
@@ -50,52 +54,98 @@ func NewGraph[T comparable](directed bool) *Graph[T] {
 	}
 }
 
-// TODO: func (g *Graph[T]) Transpose() *Graph[T] { return nil }
+func (g *Graph[T]) Transpose() *Graph[T] {
+	if !g.directed {
+		return g
+	}
+
+	t := &Graph[T]{
+		edges:    make(map[T]map[T]int, g.eCnt),
+		vertices: g.vertices, // maybe should make a copy of vertices
+		directed: g.directed,
+	}
+
+	for uID := range g.edges {
+		for vID := range g.edges[uID] {
+			t.AddEdge(vID, uID, g.edges[uID][vID])
+		}
+	}
+
+	return t
+}
 
 func (g *Graph[T]) Vertex(id T) (*Node[T], bool) {
 	node, exists := g.vertices[id]
+
 	return node, exists
 }
 
-func (g *Graph[T]) Edge(uID, vID T) (*Edge[T], bool) {
-	if _, exists := g.edges[uID][vID]; !exists {
-		return nil, false
-	}
-
-	bound := "->"
-	if !g.directed {
-		bound = "--"
-	}
-
-	return &Edge[T]{
-		u:     g.vertices[uID],
-		v:     g.vertices[vID],
-		w:     g.edges[uID][vID],
-		bound: bound,
-	}, true
-}
-
 func (g *Graph[T]) Vertices() []*Node[T] {
-	verteces := make([]*Node[T], 0, g.Order())
+	verteces := make([]*Node[T], 0, g.vCnt)
+
 	for _, v := range g.vertices {
 		verteces = append(verteces, v)
 	}
+
 	return verteces
 }
 
 func (g *Graph[T]) AddVertex(id T) bool {
-	if _, exists := g.vertices[id]; exists {
-		return false
-	}
-	g.vertices[id] = NewNode(id)
-	if _, exists := g.edges[id]; !exists {
+	var exists bool
+
+	if _, exists = g.edges[id]; !exists {
 		g.edges[id] = make(map[T]int)
 	}
+
+	if _, exists = g.vertices[id]; exists {
+		return false
+	}
+
+	g.vertices[id] = NewNode(id)
+	g.vCnt++
+
 	return true
 }
 
-func (g *Graph[T]) Order() int {
-	return len(g.vertices)
+// func (g *Graph[T]) Edge(uID, vID T) (*Edge[T], bool) {
+// 	if _, exists := g.edges[uID][vID]; !exists {
+// 		return nil, false
+// 	}
+//
+// 	bound := "->"
+// 	if !g.directed {
+// 		bound = "--"
+// 	}
+//
+// 	return &Edge[T]{
+// 		u:     g.vertices[uID],
+// 		v:     g.vertices[vID],
+// 		w:     g.edges[uID][vID],
+// 		bound: bound,
+// 	}, true
+// }
+
+func (g *Graph[T]) Edges() []*Edge[T] {
+	edges := make([]*Edge[T], 0, g.eCnt)
+	bound := "->"
+
+	if !g.directed {
+		bound = "--"
+	}
+
+	for uID := range g.edges {
+		for vID := range g.edges[uID] {
+			e := &Edge[T]{
+				u:     g.vertices[uID],
+				v:     g.vertices[vID],
+				w:     g.edges[uID][vID],
+				bound: bound,
+			}
+			edges = append(edges, e)
+		}
+	}
+
+	return edges
 }
 
 func (g *Graph[T]) AddEdge(uID, vID T, w int) {
@@ -103,17 +153,26 @@ func (g *Graph[T]) AddEdge(uID, vID T, w int) {
 	g.AddVertex(vID)
 
 	g.edges[uID][vID] = w
+	g.eCnt++
+
 	if !g.directed {
 		g.edges[vID][uID] = w
+		g.eCnt++
 	}
 }
 
 func (g *Graph[T]) Neighbours(id T) []*Node[T] {
 	neighbours := make([]*Node[T], 0, g.Degree(id))
+
 	for nID := range g.edges[id] {
 		neighbours = append(neighbours, g.vertices[nID])
 	}
+
 	return neighbours
+}
+
+func (g *Graph[T]) Order() int {
+	return g.vCnt
 }
 
 func (g *Graph[T]) Degree(id T) int {
@@ -148,6 +207,8 @@ func (g Graph[T]) String() string {
 		sb.WriteString("],\n")
 	}
 
-	sb.WriteByte(']')
+	sb.WriteString("]\n")
+	fmt.Fprintf(&sb, "vertices: %d\nedges: %d", g.vCnt, g.eCnt)
+
 	return sb.String()
 }
