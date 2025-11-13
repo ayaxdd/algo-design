ea2215ebaf3d5a3225584ef700e7d047272a281b
arc <zaveyborodaid@gmail.com>
2025-11-13 20:44:48 +0500

    test

--- main.go
+++ main.go
@@ -4,27 +4,34 @@ import (
 	"fmt"
 
 	"github.com/ayaxdd/algorithm-design/collection"
-	"github.com/ayaxdd/algorithm-design/graph"
+	"github.com/ayaxdd/algorithm-design/iterator"
 )
 
 func main() {
 	g := collection.NewGraph[int](true)
 
-	g.AddEdge(0, 1, 1)
-	g.AddEdge(0, 2, 1)
 	g.AddEdge(0, 4, 1)
+	g.AddEdge(1, 0, 1)
 	g.AddEdge(1, 3, 1)
-	g.AddEdge(1, 4, 1)
-	g.AddEdge(2, 3, 1)
-	g.AddEdge(2, 4, 9)
-	g.AddEdge(3, 4, 8)
-	g.AddEdge(4, 0, 8)
+	g.AddEdge(1, 5, 1)
+	g.AddEdge(2, 1, 1)
+	g.AddEdge(3, 7, 1)
+	g.AddEdge(5, 1, 1)
+	g.AddEdge(5, 3, 1)
+	g.AddEdge(6, 4, 9)
+	g.AddEdge(6, 5, 8)
+	g.AddEdge(7, 5, 8)
 	fmt.Println(g)
 
-	for _, v := range g.Vertices() {
-		fmt.Printf("%v: in=%d; out=%d", v, v.InDegree(), v.OutDegree())
-		fmt.Println()
+	bfs := iterator.NewBFSIterator(g, 0)
+	dfs := iterator.NewDFSIterator(g, 5)
+
+	for v := range dfs {
+		fmt.Println(v)
 	}
 
-	fmt.Println(graph.DfsSort(g))
+	fmt.Println("fdfd")
+	for v := range bfs {
+		fmt.Println(v)
+	}
 }
