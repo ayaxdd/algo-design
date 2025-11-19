59e45785226f1ec5389da57416f8da9ae992378f
arc <zaveyborodaid@gmail.com>
2025-11-19 15:00:06 +0500

    Implement AllIntervalScheduling

--- greedy/scheduling.go
+++ greedy/scheduling.go
@@ -12,7 +12,7 @@ type Interval struct {
 }
 
 func IntervalScheduling(intervals []Interval) []Interval {
-	if len(intervals) < 0 {
+	if len(intervals) == 0 {
 		return intervals
 	}
 
@@ -48,6 +48,59 @@ func IntervalScheduling(intervals []Interval) []Interval {
 	return result
 }
 
+// returns map where key is resource label, value is intervals that attached to this resource
+
+func AllIntervalScheduling(intervals []Interval) map[int][]Interval {
+	if len(intervals) == 0 {
+		return make(map[int][]Interval)
+	}
+
+	result := make(map[int][]Interval)
+	deep := make(map[int]int) // key: label, value: finish of last interval
+	label := 0
+
+	fmt.Println("before sorting:")
+	fmt.Println(intervals)
+
+	// sort by start
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+
+	fmt.Println("after sorting:")
+	fmt.Println(intervals)
+
+	for j := range intervals {
+		s := intervals[j].Start
+		f := intervals[j].Finish
+		found := false
+
+		for l := 1; l < label; l++ {
+			if fl, ok := deep[l]; ok && s >= fl {
+				deep[l] = f
+				result[l] = append(result[l], intervals[j])
+				found = true
+
+				break
+			}
+		}
+
+		if !found {
+			label++
+			deep[label] = f
+			result[label] = append(result[label], intervals[j])
+		}
+	}
+
+	fmt.Println("after scheduling:")
+
+	for k, v := range result {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	return result
+}
+
 func TestIntervals() {
 	intervals := make([]Interval, 0, 10)
 
@@ -57,5 +110,5 @@ func TestIntervals() {
 		intervals = append(intervals, Interval{Start: s, Finish: f})
 	}
 
-	IntervalScheduling(intervals)
+	AllIntervalScheduling(intervals)
 }
