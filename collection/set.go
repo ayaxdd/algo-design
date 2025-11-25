97e02d87c71b8097cb962dc6012fb8e8554a8dba
arc <zaveyborodaid@gmail.com>
2025-11-25 22:58:50 +0500

    Fix NewSet() func

--- collection/set.go
+++ collection/set.go
@@ -10,8 +10,14 @@ type (
 	Set[T comparable] map[T]stub
 )
 
-func NewSet[T comparable]() Set[T] {
-	return make(Set[T])
+func NewSet[T comparable](items ...T) Set[T] {
+	s := make(Set[T], len(items))
+
+	for _, item := range items {
+		s[item] = true
+	}
+
+	return s
 }
 
 func (s Set[T]) Add(items ...T) {
@@ -32,16 +38,10 @@ func (s Set[T]) Contains(item T) bool {
 	return exists
 }
 
-func (s Set[T]) Iterate(f func(T)) {
-	for k := range s {
-		f(k)
-	}
-}
-
 func (s Set[T]) Clone() Set[T] {
 	cp := make(Set[T], s.Len())
 
-	s.Iterate(func(k T) {
+	s.iterate(func(k T) {
 		cp.Add(k)
 	})
 
@@ -63,7 +63,7 @@ func (s Set[T]) Clear() {
 func Union[T comparable](a, b Set[T]) Set[T] {
 	res := a.Clone()
 
-	b.Iterate(func(k T) {
+	b.iterate(func(k T) {
 		res.Add(k)
 	})
 
@@ -77,7 +77,7 @@ func Intersection[T comparable](a, b Set[T]) Set[T] {
 
 	res := make(Set[T])
 
-	a.Iterate(func(k T) {
+	a.iterate(func(k T) {
 		if b.Contains(k) {
 			res.Add(k)
 		}
@@ -89,13 +89,19 @@ func Intersection[T comparable](a, b Set[T]) Set[T] {
 func Difference[T comparable](a, b Set[T]) Set[T] {
 	res := make(Set[T])
 
-	a.Iterate(func(k T) {
+	a.iterate(func(k T) {
 		if !b.Contains(k) {
 			res.Add(k)
 		}
 	})
 
 	return res
+}
+
+func (s Set[T]) iterate(f func(T)) {
+	for k := range s {
+		f(k)
+	}
 }
 
 func (s Set[T]) String() string {
